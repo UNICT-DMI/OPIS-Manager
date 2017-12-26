6f0b01ee34bce50d4dbc67d35b2ba6e771bea013
Helias <stefanoborzi32@gmail.com>
2017-12-26 18:10:03 +0100

    Updated opis_structure.sql, added static_data.js

---
+++ data/static_data.js
@@ -0,0 +1,25 @@
+/* static data for frontend */
+const questions = [
+  "Le conoscenze preliminari sono state sufficienti per la comprensione degli argomenti nel programma d'esame?",
+  "Il carico di studio dell'insegnamento è proporzionato ai crediti assegnati?",
+  "Il materiale didattico (indicato e disponibile) è adeguato per lo studio della materia?",
+  "Le modalità di esame sono state definite in modo chiaro?",
+  "Gli orari di svolgimento di lezioni, esercitazioni e altre eventuali attività didattiche sono rispettati?",
+  "Il docente stimola/motiva l'interesse verso la disciplina?",
+  "Il docente espone gli argomenti in modo chiaro?",
+  "Le attività didattiche integrative (esercitazioni, tutorati, laboratori, ... etc) sono utili all'apprendimento della materia?"; // (risposte al netto di coloro che hanno indicato 'non previste')
+  "L'insegnamento è stato svolto in maniera coerente con quanto dichiarato sul sito web del corso di studio?",
+  "Il docente è reperibile per chiarimenti e spiegazioni?",
+  "E' interessato/a agli argomenti trattati nell'insegnamento?",
+  "E' complessivamente soddisfatto/a dell'insegnamento?"
+];
+
+const reasons = [
+  "Lavoro",
+  "Frequenza lezioni di altri insegnamenti",
+  "Frequenza poco utile ai fine della preparazione dell'esame",
+  "Strutture per l'attività didattica inadeguate per la frequenza",
+  "Ritardo negli studi",
+  "Problemi logistici",
+  "Altro"
+];
