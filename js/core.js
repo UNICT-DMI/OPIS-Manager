5ca79cf9f6bd416077ee4afcea83b35f9620df40
Helias <stefanoborzi32@gmail.com>
2017-12-26 22:29:14 +0100

    Added frontend draft

---
+++ js/core.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  const api_url = "API/public/index.php/api/";
+
+  $.getJSON( api_url+"dipartimenti", function( data ) {
+
+    var dip = $("#dipartimenti");
+
+    $.each(data, function(key, val) {
+      dip.append(new Option(data[key].nome, data[key].id));
+    });
+
+  });
+
+});
---
+++ setup_api.sh
@@ -0,0 +1,9 @@
+chmod 777 -R API/storage/
+chmod 777 -R API/storage/logs/
+chmod 777 -R API/storage/framework/cache
+chmod 777 -R API/storage/framework/views
+chmod 777 -R API/storage/framework/sessions
+chmod 777 -R API/bootstrap/cache
+
+cp API/.env.example API/.env
+
