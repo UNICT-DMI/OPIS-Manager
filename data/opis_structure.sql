7994f530eb905f4abc3867f772ac0ab2f44a247e
Helias <stefanoborzi32@gmail.com>
2017-12-26 20:20:15 +0100

    Updated opis_structure.sql

--- data/opis_structure.sql
+++ data/opis_structure.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS `dipartimento` (
     `id` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `name` VARCHAR(255) NOT NULL,
+    `tot_cds` INT(11) NOT NULL,
     `tot_moduli` INT(11) NOT NULL,
     `tot_valutati` INT(11) NOT NULL,
     `tot_schedeF` INT(11) NOT NULL,
-    `tot_schedeNF` INT(11) NOT NULL,
-    `id_opis` INT(11) NOT NULL
+    `tot_schedeNF` INT(11) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `cds` (
