bc67572007ab4833ed16b8ad251e70caf1c7538d
Pierpaolo Pecoraio <pierpaolo.pecoraio@gmail.com>
2017-12-30 16:59:45 +0100

    added id_dipartimento and varius changes

--- scrapers/cds.php
+++ scrapers/cds.php
@@ -1,5 +1,81 @@
 <?php
+$x=379;
+insegnamento($x);
+function cds($id_dip)
+{
+    include 'methods.php';
+    include 'globalVar.php';
+    include 'config.php';
+    $mysqli = new mysqli($host, $username, $password, $db_name);
+    if ($mysqli->connect_error)
+        die('Errore di connessione');
+
+    $query = "INSERT INTO cds (id, nome,classe,tot_moduli,tot_valutati,tot_schedeF,tot_schedeNF,id_dipartimento) VALUES\n";
+
+    $xpath   = new DOMXPath(getDOM($link .'cds_dip.php?id_dip='.$id_dip));
+    $lengthN = $xpath->query('/html/body/table[2]/tr/td/table/tr')->length;
+
+    for ($j = 2; $j < $lengthN; $j++) {
+
+        $_cod_corso       = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[2]')->item(0)->textContent;
+        $linkDipartimento = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[3]/a')->item(0)->attributes->item(0)->textContent;
+        $_nome            = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[3]')->item(0)->textContent;
+
+        $_classe = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[4]')->item(0)->textContent;
+
+        $_tot_moduli = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[5]')->item(0)->textContent;
+
+        $_tot_valutati = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[6]')->item(0)->textContent;
+
+        $_tot_schedeF = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[8]')->item(0)->textContent;
+
+        $_tot_schedeNF = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[9]')->item(0)->textContent;
+
+        $link_opis = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $j . ']/td[10]/a')->item(0)->attributes->item(0)->textContent; // Link OPIS
+        if (!$mysqli->query('SELECT id FROM cds WHERE id=' . $_cod_corso . ';')->num_rows) {
+            $query .= '("' . addslashes($_cod_corso) . '", "' . addslashes($_nome) . '", "' . addslashes($_classe) . '", "' . addslashes($_tot_moduli) . '", "' . addslashes($_tot_valutati) . '", "' . addslashes($_tot_schedeF) . '","' . addslashes($_tot_schedeNF) . '","' . addslashes($id_dip) . '"),';
+            $query .= "\n";
+
+        }
+    }
 
 
+    $query = substr($query, 0, -2);
+    $query .= ";";
+    $endQuery = substr($query, strlen($query) - 6);
 
+    if ($endQuery != 'VALUE;')
+        if (!$mysqli->query($query))
+            die($mysqli->error);
+
+}
+
+function insegnamento($id_cds) {
+  include 'methods.php';
+  include 'globalVar.php';
+  include 'config.php';
+
+  $mysqli = new mysqli($host, $username, $password, $db_name);
+  if ($mysqli->connect_error)
+      die('Errore di connessione');
+
+  else {
+
+        $xpath   = new DOMXPath(getDOM($link .'insegn_cds.php?cod_corso='.$id_cds));
+        $lengthN = $xpath->query('/html/body/table[2]/tr/td/table/tr')->length;
+        echo $lengthN;
+  }
+}
+function getDOM($link)
+{
+    libxml_use_internal_errors(true);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $link);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $html = curl_exec($ch);
+    $dom  = new DOMDocument;
+    $dom->loadHTML($html);
+    curl_close($ch);
+    return $dom;
+}
  ?>
--- scrapers/dipartimento.php
+++ scrapers/dipartimento.php
@@ -1,115 +1,83 @@
 <?php
-    include 'cds.php';
-    cds(dip());
-    function dip() {
-      include 'config.php';
-      include 'globalVar.php';
-      include 'methods.php';
-      $arr = array();
-      $mysqli = new mysqli($host, $username, $password, $db_name);
+include 'cds.php';
+dip();
+function dip()
+{
+    include 'config.php';
+    include 'globalVar.php';
 
-      if ($mysqli->connect_error)
+    $arr    = array();
+    $mysqli = new mysqli($host, $username, $password, $db_name);
+
+    if ($mysqli->connect_error)
         die('Errore di connessione');
 
-
-      else {
-        //$mysqli->select_db($db_name);
-        $xpath = new DOMXPath(getDOM($link));
+    else {
+        $xpath   = new DOMXPath(getDOM($link));
         $lengthN = $xpath->query('/html/body/table[2]/tr/td/table/tr')->length; // Lunghezza righe
 
-  			$query = "INSERT INTO dipartimento (name, tot_moduli,tot_valutati,tot_schedeF,tot_schedeNF) VALUES\n";
-        $j = 0;
+        $query = "INSERT INTO dipartimento (id,nome, tot_cds, tot_moduli,tot_valutati,tot_schedeF,tot_schedeNF) VALUES\n";
+
         for ($i = 2; $i < $lengthN; $i++) {
 
-                $linkDipartimento = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[1]/a')->item(0)->attributes->item(0)->textContent; //Link Dipartimento
-                $_nome = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[1]')->item(0)->textContent; // Nome dipartimento
+            $linkDipartimento = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[1]/a')->item(0)->attributes->item(0)->textContent; //Link Dipartimento
+            $_nome            = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[1]')->item(0)->textContent; // Nome dipartimento
 
-                $_tot_CdS = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[2]')->item(0)->textContent; // Tot CdS
+            $_id = parseID($linkDipartimento);
 
-                $_tot_moduli = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[3]')->item(0)->textContent; // Tot Moduli
+            $_tot_CdS = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[2]')->item(0)->textContent; // Tot CdS
 
-                $_tot_valutati = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[4]')->item(0)->textContent; // Tot Valutati
+            $_tot_moduli = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[3]')->item(0)->textContent; // Tot Moduli
 
-                $_tot_schedeF= $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[6]')->item(0)->textContent; // Totale schede frequentanti
+            $_tot_valutati = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[4]')->item(0)->textContent; // Tot Valutati
 
-                $_tot_schedeNF= $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[7]')->item(0)->textContent; // Totale schede NON frequentanti
+            $_tot_schedeF = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[6]')->item(0)->textContent; // Totale schede frequentanti
 
-                $link_opis = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$i.']/td[8]/a')->item(0)->attributes->item(0)->textContent; // Link OPIS
+            $_tot_schedeNF = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[7]')->item(0)->textContent; // Totale schede NON frequentanti
 
-                $arr[$j] = $linkDipartimento;
-                $aPathOpis_Dipartimento[$j] = $link_opis;
+            $link_opis = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[8]/a')->item(0)->attributes->item(0)->textContent; // Link OPIS
 
-                $j++;
+            //$aPathOpis_Dipartimento[$] = $link_opis;
+            cds($_id);
 
-                $query .= '("' . addslashes($_nome) . '", "' . addslashes($_tot_moduli) . '", "' . addslashes($_tot_valutati) . '", "' . addslashes($_tot_schedeF) . '", "' . addslashes($_tot_schedeNF) . '"),';
+            if (!$mysqli->query('SELECT id FROM dipartimento WHERE id=' . $_id . ';')->num_rows) {
+                $query .= '("' . addslashes($_id) . '","' . addslashes($_nome) . '","' . addslashes($_tot_CdS) . '", "' . addslashes($_tot_moduli) . '", "' . addslashes($_tot_valutati) . '", "' . addslashes($_tot_schedeF) . '", "' . addslashes($_tot_schedeNF) . '"),';
                 $query .= "\n";
+            }
 
         }
 
-  			$query = substr($query, 0, -2);
-  			$query .= ";";
-
-  //			echo "\n" . $query . "\n";
-
-  			if (!$mysqli->query($query))
-  				die($mysqli->error);
-
-        return $arr;
-      }
-    }
-    function getDOM($link){
-        libxml_use_internal_errors(true);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $link);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $html = curl_exec($ch);
-        $dom = new DOMDocument;
-        $dom->loadHTML($html);
-        curl_close($ch);
-        return $dom;
-    }
-    function cds($aPath) {
-      include 'methods.php';
-      include 'globalVar.php';
-      include 'config.php';
-      $mysqli = new mysqli($host, $username, $password, $db_name);
-      if ($mysqli->connect_error)
-        die('Errore di connessione');
-        echo "ciao";
-
-        $query = "INSERT INTO cds (id, name,class,tot_moduli,tot_valutati,tot_schedeF,tot_schedeNF,id_dipartimento) VALUES\n";
-        for ($i=0; $i<count($aPath); $i++) {
-          $xpath = new DOMXPath(getDOM($link.$aPath[$i]));
-          $lengthN = $xpath->query('/html/body/table[2]/tr/td/table/tr')->length;
-
-          for ($j=2; $j<$lengthN; $j++) {
-
-            $_cod_corso =  $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[2]')->item(0)->textContent;
-            $linkDipartimento = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[3]/a')->item(0)->attributes->item(0)->textContent;
-            $_nome = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[3]')->item(0)->textContent; // Nome dipartimento
-
-            $_classe = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[4]')->item(0)->textContent; // Tot CdS
-
-            $_tot_moduli = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[5]')->item(0)->textContent; // Tot CdS
-
-            $_tot_valutati = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[6]')->item(0)->textContent; // Tot Valutati
-
-            $_tot_schedeF= $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[8]')->item(0)->textContent; // Totale schede frequentanti
-
-            $_tot_schedeNF= $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[9]')->item(0)->textContent; // Totale schede NON frequentanti
-
-            $link_opis = $xpath->query('/html/body/table[2]/tr/td/table/tr['.$j.']/td[10]/a')->item(0)->attributes->item(0)->textContent; // Link OPIS
-
-            $query .= '("' . addslashes($_cod_corso) . '", "' . addslashes($_nome) . '", "' . addslashes($_classe) . '", "' . addslashes($_tot_moduli) . '", "' . addslashes($_tot_valutati) . '", "' . addslashes($_tot_schedeF) . '","' . addslashes($_tot_schedeNF) . '","' . addslashes($i) . '"),';
-            $query .= "\n";
-
-          }
-
         $query = substr($query, 0, -2);
         $query .= ";";
-        if (!$mysqli->query($query))
-          die($mysqli->error);
-      }
+
+        $endQuery = substr($query, strlen($query) - 6);
+        if ($endQuery != 'VALUE;')
+            if (!$mysqli->query($query))
+                die($mysqli->error);
+        return $arr;
     }
+}
+
+function parseID($str)
+{
+    $x = substr($str, strlen($str) - 2);
+    if ($x[0] == '=') {
+        $x = $x[1];
+    }
+    return $x;
+}
+
+function getDOM($link)
+{
+    libxml_use_internal_errors(true);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $link);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $html = curl_exec($ch);
+    $dom  = new DOMDocument;
+    $dom->loadHTML($html);
+    curl_close($ch);
+    return $dom;
+}
 
 ?>
