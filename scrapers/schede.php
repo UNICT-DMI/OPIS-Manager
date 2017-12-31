e5b6d280fff34bdba9bab67dd13836a040454ee6
Helias <stefanoborzi32@gmail.com>
2017-12-31 21:01:38 +0100

    Added extractors for graphs

--- scrapers/schede.php
+++ scrapers/schede.php
@@ -28,6 +28,43 @@ function schede($id_cds, $id_gomp) {
   $fuoriCorso_nf    = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[4]')->item(0)->textContent;
   $inattivi_nf      = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[5]')->item(0)->textContent;
 
+  /* data from graphs */
+  $eta = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr/td//img')->item(0)->getAttribute("src");
+  $eta = str_replace("./graph_eta.php?", "", $eta);
+  $eta = str_replace("eta", "", $eta);
+  $eta = str_replace("1=", "", $eta);
+  $eta = str_replace("2=", "", $eta);
+  $eta = str_replace("3=", "", $eta);
+  $eta = str_replace("4=", "", $eta);
+  $eta = str_replace("5=", "", $eta);
+  $eta = str_replace("6=", "", $eta);
+  $eta = str_replace("7=", "", $eta);
+  $eta = explode("&", $eta);
+
+  $eta[0] = array("18-19",      $eta[0]);
+  $eta[1] = array("20-21",      $eta[1]);
+  $eta[2] = array("22-23",      $eta[2]);
+  $eta[3] = array("24-25",      $eta[3]);
+  $eta[4] = array("26-27",      $eta[4]);
+  $eta[5] = array("28-29",      $eta[5]);
+  $eta[6] = array("30 e oltre", $eta[6]);
+
+  $anno_iscr = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr/td[2]//img')->item(0)->getAttribute("src");
+  $anno_iscr = str_replace("./graph_annoiscr.php?", "", $anno_iscr);
+  $anno_iscr = str_replace("iscr", "", $anno_iscr);
+  $anno_iscr = str_replace("1=", "", $anno_iscr);
+  $anno_iscr = str_replace("2=", "", $anno_iscr);
+  $anno_iscr = str_replace("3=", "", $anno_iscr);
+  $anno_iscr = str_replace("4=", "", $anno_iscr);
+  $anno_iscr = str_replace("5=", "", $anno_iscr);
+  $anno_iscr = str_replace("6=", "", $anno_iscr);
+  $anno_iscr = str_replace("FC=", "", $anno_iscr);
+  $anno_iscr = explode("&", $anno_iscr);
+
+  print_r($anno_iscr);
+
+  die();
+
   // questions answers
   $questions = array();
   for ($i = 2; $i < 14; $i++)
@@ -70,8 +107,6 @@ function schede($id_cds, $id_gomp) {
         $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[1]')->item(0)->textContent,
         $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[2]')->item(0)->textContent
       );
-
-  // var_dump($suggestions_nf);
 }
 schede(346, 72442);
 ?>
