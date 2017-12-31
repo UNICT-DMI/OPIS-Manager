6e16cc194b4e44bb29e751b92e10f674b390bba0
Helias <stefanoborzi32@gmail.com>
2017-12-31 19:50:25 +0100

    Updated schede.php

--- scrapers/schede.php
+++ scrapers/schede.php
@@ -28,22 +28,50 @@ function schede($id_cds, $id_gomp) {
   $fuoriCorso_nf    = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[4]')->item(0)->textContent;
   $inattivi_nf      = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[5]')->item(0)->textContent;
 
-  // Extract questions answers
+  // questions answers
   $questions = array();
-  for ($i = 1; $i <= 12; $i++)
-    for ($j = 1; $j <= 5; $j++)
-      $questions[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td/table[1]/tr[' . ($i+1) . ']/td[' . ($j+1) . ']')->item(0)->textContent;
+  for ($i = 2; $i < 14; $i++)
+    for ($j = 2; $j < 7; $j++)
+      $questions[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td/table[1]/tr[' . $i . ']/td[' . $j . ']')->item(0)->textContent;
 
-  // Extract questions answers (nf)
+  // questions answers (nf)
   $questions_nf = array();
-  for ($i = 1; $i <= 12; $i++)
-    for ($j = 1; $j <= 5; $j++)
-      $questions_nf[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td[2]/table[1]/tr[' . ($i+1) . ']/td[' . ($j+1) . ']')->item(0)->textContent;
+  for ($i = 2; $i < 14; $i++) {
+    if ($i == 6)
+      $i += 5;
+
+    for ($j = 2; $j < 7; $j++)
+      $questions_nf[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td[2]/table[1]/tr[' . $i . ']/td[' . $j . ']')->item(0)->textContent;
+  }
 
   for ($i = 20; $i < 45; $i++)
     $questions_nf[$i] = "dom. non prevista";
 
-  var_dump($questions_nf);
+  // reasons nf
+  $motivi_nf = array();
+  for ($i = 2; $i < 9; $i++)
+    $motivi_nf[] = array(
+      $xpath->query('/html/body/table[1]/tr/td/table[6]/td[2]/table/tr[' . $i . ']/td[1]')->item(0)->textContent,
+      $xpath->query('/html/body/table[1]/tr/td/table[6]/td[2]/table/tr[' . $i . ']/td[2]')->item(0)->textContent
+    );
+
+  // suggestions
+  $suggestions = array();
+  for ($i = 2; $i < 12; $i++)
+      $suggestions[] = array(
+        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[1]/table[1]/tr[' . $i . ']/td[1]')->item(0)->textContent,
+        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[1]/table[1]/tr[' . $i . ']/td[2]')->item(0)->textContent
+      );
+
+  // suggestions nf
+  $suggestions_nf = array();
+  for ($i = 2; $i < 12; $i++)
+      $suggestions_nf[] = array(
+        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[1]')->item(0)->textContent,
+        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[2]')->item(0)->textContent
+      );
+
+  // var_dump($suggestions_nf);
 }
 schede(346, 72442);
 ?>
