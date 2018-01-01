7538020f057e6ef5a7999c6e8ea5ea5d1ededb6c
Helias <stefanoborzi32@gmail.com>
2018-01-02 00:11:11 +0100

    Updated "schede" table, finished schede.php scraper

--- scrapers/schede.php
+++ scrapers/schede.php
@@ -12,10 +12,40 @@ function getDOM($link)
     return $dom;
 }
 
-function schede($id_cds, $id_gomp) {
+function filter_graph($url, $fileName, $fieldName) {
+
+  $url = str_replace("./" . $fileName . ".php?", "", $url);
+  $url = str_replace($fieldName, "", $url);
+
+  $url = str_replace("1=", "", $url);
+  $url = str_replace("2=", "", $url);
+  $url = str_replace("3=", "", $url);
+  $url = str_replace("4=", "", $url);
+  $url = str_replace("5=", "", $url);
+  $url = str_replace("6=", "", $url);
+  $url = str_replace("7=", "", $url);
+  $url = str_replace("8=", "", $url);
+  $url = str_replace("9=", "", $url);
+  $url = str_replace("10=", "", $url);
+  $url = str_replace("FC=", "", $url);
+
+  $url = explode("&", $url);
+
+  return $url;
+}
+
+function fill_arr($arr, $elements) {
+  foreach ($arr as $key=>$value)
+    $arr[$key] = array($elements[$key], $arr[$key]);
+
+  return $arr;
+}
+
+function schede($id_cds, $id_gomp, $cod_modulo, $canale) {
   $link = "http://www.rett.unict.it/nucleo/val_did/anno_1617/"; // const
 
   $url = $link . "val_insegn.php?cod_corso=" . $id_cds . "&cod_gomp=" . $id_gomp . "&cod_modulo=0&canale=no";
+
   $xpath = new DOMXPath(getDOM($url));
 
   $totaleSchede     = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[2]/td[2]')->item(0)->textContent;
@@ -24,65 +54,54 @@ function schede($id_cds, $id_gomp) {
   $inattivi         = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[2]/td[5]')->item(0)->textContent;
 
   $totaleSchede_nf  = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[2]')->item(0)->textContent;
-  $femmine_nf       = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[3]')->item(0)->textContent; // non prevista
+  $femmine_nf       = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[3]')->item(0)->textContent; // "non prevista"
   $fuoriCorso_nf    = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[4]')->item(0)->textContent;
   $inattivi_nf      = $xpath->query('/html/body/table[1]/tr/td/table[4]/tr[3]/td[5]')->item(0)->textContent;
 
   /* data from graphs */
   $eta = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr/td//img')->item(0)->getAttribute("src");
-  $eta = str_replace("./graph_eta.php?", "", $eta);
-  $eta = str_replace("eta", "", $eta);
-  $eta = str_replace("1=", "", $eta);
-  $eta = str_replace("2=", "", $eta);
-  $eta = str_replace("3=", "", $eta);
-  $eta = str_replace("4=", "", $eta);
-  $eta = str_replace("5=", "", $eta);
-  $eta = str_replace("6=", "", $eta);
-  $eta = str_replace("7=", "", $eta);
-  $eta = explode("&", $eta);
-
-  $eta[0] = array("18-19",      $eta[0]);
-  $eta[1] = array("20-21",      $eta[1]);
-  $eta[2] = array("22-23",      $eta[2]);
-  $eta[3] = array("24-25",      $eta[3]);
-  $eta[4] = array("26-27",      $eta[4]);
-  $eta[5] = array("28-29",      $eta[5]);
-  $eta[6] = array("30 e oltre", $eta[6]);
+  $eta = filter_graph($eta, "graph_eta", "eta");
+  $eta = fill_arr($eta, array("18-19","20-21","22-23","24-25","26-27","28-29","30 e oltre"));
 
   $anno_iscr = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr/td[2]//img')->item(0)->getAttribute("src");
-  $anno_iscr = str_replace("./graph_annoiscr.php?", "", $anno_iscr);
-  $anno_iscr = str_replace("iscr", "", $anno_iscr);
-  $anno_iscr = str_replace("1=", "", $anno_iscr);
-  $anno_iscr = str_replace("2=", "", $anno_iscr);
-  $anno_iscr = str_replace("3=", "", $anno_iscr);
-  $anno_iscr = str_replace("4=", "", $anno_iscr);
-  $anno_iscr = str_replace("5=", "", $anno_iscr);
-  $anno_iscr = str_replace("6=", "", $anno_iscr);
-  $anno_iscr = str_replace("FC=", "", $anno_iscr);
-  $anno_iscr = explode("&", $anno_iscr);
+  $anno_iscr = filter_graph($anno_iscr, "graph_annoiscr", "iscr");
+  $anno_iscr = fill_arr($anno_iscr, array("1", "2", "3", "4", "5", "6", "FC"));
 
-  print_r($anno_iscr);
+  $n_studenti = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr[2]/td[1]//img')->item(0)->getAttribute("src");
+  $n_studenti = filter_graph($n_studenti, "graph_stud_freq", "stud");
+  $n_studenti = fill_arr($n_studenti, array("fino 25", "26-50", "51-75", "76-100", "101-151", "151-200", "oltre 200"));
 
-  die();
+  $ragg_uni = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr[2]/td[2]//img')->item(0)->getAttribute("src");
+  $ragg_uni = filter_graph($ragg_uni, "graph_tempo_univ", "tmp");
+  $ragg_uni = fill_arr($ragg_uni, array("fino 0.5", "0.5-1", "1-2", "2-3", "oltre 3"));
+
+  $studio_gg = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr[3]/td[1]//img')->item(0)->getAttribute("src");
+  $studio_gg = filter_graph($studio_gg, "graph_ore_studio_gg", "ore");
+  $studio_gg = fill_arr($studio_gg, array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"));
+
+  $studio_tot = $xpath->query('/html/body/table[1]/tr/td/table[5]/tr[3]/td[2]//img')->item(0)->getAttribute("src");
+  $studio_tot = str_replace("&ore8=0", "", $studio_tot);
+  $studio_tot = filter_graph($studio_tot, "graph_ore_studio_tot", "ore");
+  $studio_tot = fill_arr($studio_tot, array("fino 50", "51-100", "101-150", "201-250", "251-300", "301-350", "oltre 300"));
 
   // questions answers
-  $questions = array();
+  $domande = array();
   for ($i = 2; $i < 14; $i++)
     for ($j = 2; $j < 7; $j++)
-      $questions[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td/table[1]/tr[' . $i . ']/td[' . $j . ']')->item(0)->textContent;
+      $domande[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td/table[1]/tr[' . $i . ']/td[' . $j . ']')->item(0)->textContent;
 
   // questions answers (nf)
-  $questions_nf = array();
+  $domande_nf = array();
   for ($i = 2; $i < 14; $i++) {
     if ($i == 6)
       $i += 5;
 
     for ($j = 2; $j < 7; $j++)
-      $questions_nf[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td[2]/table[1]/tr[' . $i . ']/td[' . $j . ']')->item(0)->textContent;
+      $domande_nf[] = $xpath->query('/html/body/table[1]/tr/td/table[6]/tr/td[2]/table[1]/tr[' . $i . ']/td[' . $j . ']')->item(0)->textContent;
   }
 
   for ($i = 20; $i < 45; $i++)
-    $questions_nf[$i] = "dom. non prevista";
+    $domande_nf[$i] = "dom. non prevista";
 
   // reasons nf
   $motivi_nf = array();
@@ -93,20 +112,61 @@ function schede($id_cds, $id_gomp) {
     );
 
   // suggestions
-  $suggestions = array();
+  $sugg = array();
   for ($i = 2; $i < 12; $i++)
-      $suggestions[] = array(
-        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[1]/table[1]/tr[' . $i . ']/td[1]')->item(0)->textContent,
-        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[1]/table[1]/tr[' . $i . ']/td[2]')->item(0)->textContent
-      );
+    $sugg[] = array(
+      $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[1]/table[1]/tr[' . $i . ']/td[1]')->item(0)->textContent,
+      $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[1]/table[1]/tr[' . $i . ']/td[2]')->item(0)->textContent
+    );
 
   // suggestions nf
-  $suggestions_nf = array();
+  $sugg_nf = array();
   for ($i = 2; $i < 12; $i++)
-      $suggestions_nf[] = array(
-        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[1]')->item(0)->textContent,
-        $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[2]')->item(0)->textContent
-      );
+    $sugg_nf[] = array(
+      $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[1]')->item(0)->textContent,
+      $xpath->query('/html/body/table[1]/tr/td/table[6]/tr[3]/td[2]/table[1]/tr[' . $i . ']/td[2]')->item(0)->textContent
+    );
+
+
+  /* Serializing data */
+
+  // graphs
+  $eta = serialize($eta);
+  $anno_iscr = serialize($anno_iscr);
+  $n_studenti = serialize($n_studenti);
+  $ragg_uni = serialize($ragg_uni);
+  $studio_gg = serialize($studio_gg);
+  $studio_tot = serialize($studio_tot);
+
+  // questions
+  $domande = serialize($domande);
+  $domande_nf = serialize($domande_nf);
+  $motivi_nf = serialize($motivi_nf);
+  $sugg = serialize($sugg);
+  $sugg_nf = serialize($sugg_nf);
+
+  $query = "INSERT INTO `schede` (`totale_schede`, `totale_schede_nf`, `femmine`, `femmine_nf`, `fc`, `fc_nf`, `inatt`, `inatt_nf`, `eta`, `anno_iscr`, `num_studenti`, `ragg_uni`, `studio_gg`, `studio_tot`, `domande`, `domande_nf`, `motivo_nf`, `sugg`, `sugg_nf`, `id_insegnamento`, `canale`) VALUES";
+  $query .= "\n";
+  $query .= '("' . addslashes($totaleSchede) . '", "' . addslashes($totaleSchede_nf) . '", ' .
+              '"' . addslashes($femmine) . '", "' . addslashes($femmine_nf) . '", ' .
+              '"' . addslashes($fuoriCorso) . '", "' . addslashes($fuoriCorso_nf) . '", ' .
+              '"' . addslashes($inattivi) . '", "' . addslashes($inattivi_nf) . '", ' .
+              '"' . addslashes($eta) . '", ' .
+              '"' . addslashes($anno_iscr) . '", ' .
+              '"' . addslashes($n_studenti) . '", ' .
+              '"' . addslashes($ragg_uni) . '", ' .
+              '"' . addslashes($studio_gg) . '", ' .
+              '"' . addslashes($studio_tot) . '", ' .
+              '"' . addslashes($domande) . '", ' .
+              '"' . addslashes($domande_nf) . '", ' .
+              '"' . addslashes($motivi_nf) . '", ' .
+              '"' . addslashes($sugg) . '", ' .
+              '"' . addslashes($sugg_nf) . '", ' .
+              '"' . $id_gomp . '", ' .
+              '"' . $canale . '");';
+
+  echo $query;
+
 }
-schede(346, 72442);
+schede(346, 72442, 0, "no");
 ?>
