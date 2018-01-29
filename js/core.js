b89270f69a51890a5f0f4942e1d67e05f41c2b3f
Malhario <malaria@malhario-x580vn.homenet.telecomitalia.it>
2018-01-29 22:06:53 +0100

    Added tabs

--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+const api_url = "API/public/index.php/api/";
 
-  const api_url = "API/public/index.php/api/";
+$(document).ready(function() {
 
   $.getJSON(api_url + "dipartimento", function(data) {
 
@@ -14,5 +14,21 @@ $(document).ready(function() {
 });
 
 function showCds(dipartimento) {
-  alert(dipartimento);
+
+    $.getJSON(api_url + "cds/" + dipartimento , function(data) {
+
+
+      var tmp = "", tmp2 = "";
+      for (var x = 0; x < data.length; x++){
+        // tmp += '<li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#tab' + x + '" role="tab" aria-controls="tab' + x + '" aria-selected="true">Tab' + x + '</a></li>';
+        tmp += '<li class="nav-item"><a class="nav-link" id="profile' + x + '-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile' + x + '" aria-selected="false">Profile</a></li>';
+        // tmp2 = '<div class="tab-pane fade show active" id="tab' + x + '" role="tabpanel" aria-labelledby="tab' + x + '-tab">tab' + x + '</div>';
+        tmp2 = '<div class="tab-pane fade show active" id="profile' + x + '" role="tabpanel" aria-labelledby="profile' + x + '-tab">' + x + '</div>';
+      }
+
+      document.getElementById("myTab").innerHTML = tmp;
+      document.getElementById("myTabContent").innerHTML = tmp2;
+
+
+    });
 }
