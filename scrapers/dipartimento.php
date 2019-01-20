1d19ad396624add31ea259ba97c685f9759c3992
Wornairz <wornairz@gmail.com>
2019-01-20 18:17:17 +0100

    added compatibility with years prior to 2016

--- scrapers/dipartimento.php
+++ scrapers/dipartimento.php
@@ -13,31 +13,35 @@ function parseID($str)
     return $x;
 }
 
-function dip()
+function olddip() // come dip() ma serve per gli anni accademici prima del 16/17 che usano una struttura HTML diversa
 {
     global $link, $mysqli, $year;
 
     $arr      = array();
 
-    $xpath    = new DOMXPath(getDOM($link));
-    $lengthN  = $xpath->query('/html/body/table[2]/tr/td/table/tr')->length; // Lunghezza righe
-
+    $xpath    = new DOMXPath(getDOM($link)); // permette di gestire il DOM come se fosse un percorso
+    $lengthN  = $xpath->query('/html/body/table[2]/tr/td/table/tr')->length; // Lunghezza righe (numero di dipartimenti)
     $query    = "INSERT INTO dipartimento (id,nome, tot_cds, tot_moduli,tot_valutati,tot_schedeF,tot_schedeNF) VALUES\n";
 
-    for ($i = 2; $i < $lengthN; $i++) {
+    for ($i = 2; $i < $lengthN; $i++) { //prendiamo le info di ogni dipartimento
 
         $linkDipartimento = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[1]/a')->item(0)->attributes->item(0)->textContent; //Link Dipartimento
         $_nome            = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[1]')->item(0)->textContent; // Nome dipartimento
         $_id = parseID($linkDipartimento);
 
-        $_tot_CdS       = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[2]')->item(0)->textContent; // Tot CdS
+        $_tot_CdS       = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[2]')->item(0)->textContent; // Tot CdS (Corsi di Studio)
         $_tot_moduli    = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[3]')->item(0)->textContent; // Tot Moduli
-        $_tot_valutati  = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[4]')->item(0)->textContent; // Tot Valutati
-        $_tot_schedeF   = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[6]')->item(0)->textContent; // Totale schede frequentanti
-        $_tot_schedeNF  = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[7]')->item(0)->textContent; // Totale schede NON frequentanti
-        $link_opis      = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[8]/a')->item(0)->attributes->item(0)->textContent; // Link OPIS
+        $_tot_valutati  = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[5]')->item(0)->textContent; // Tot Valutati
+        $_tot_schedeF   = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[7]')->item(0)->textContent; // Totale schede frequentanti
+        $_tot_schedeNF  = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[8]')->item(0)->textContent; // Totale schede NON frequentanti
+        $link_opis      = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[9]/a');//->item(0)->attributes->item(0)->textContent; // Link OPIS
 
-        //$aPathOpis_Dipartimento[$] = $link_opis;
+        if ($_tot_schedeF == '')
+            $_tot_schedeF = 0;
+ 
+        if ($_tot_schedeNF == '')
+            $_tot_schedeNF = 0;
+
         echo "\n  ## \033[1m" . ($i-1) . "/" . ($lengthN-2) . "\033[33m\t " . $_nome . "\033[0m";
         echo "";
 
@@ -50,11 +54,11 @@ function dip()
         //    cds($_id);
         // }
 
-       cds($_id);
+        oldcds($_id); // funzione che per ogni dipartimento scorre i suoi corsi di studio
 
-        if (!$mysqli->query('SELECT id FROM dipartimento WHERE id=' . $_id . ';')->num_rows) {
+        if (!$mysqli->query('SELECT id FROM dipartimento WHERE id=' . $_id . ';')->num_rows) { //se il dipartimento NON ESISTE
             $query .= '("' . addslashes($_id) . '","' . addslashes($_nome) . '","' . addslashes($_tot_CdS) . '", "' . addslashes($_tot_moduli) . '", "' . addslashes($_tot_valutati) . '", "' . addslashes($_tot_schedeF) . '", "' . addslashes($_tot_schedeNF) . '"),';
-            $query .= "\n";
+            $query .= "\n"; // lo inseriamo nel DB
         }
 
     }
@@ -69,25 +73,81 @@ function dip()
     return $arr;
 }
 
-// $link = "http://nucleo.unict.it/val_did/anno_1314/index.php";
-// $year = "2013/2014";
-// dip();
 
-// $link = "http://nucleo.unict.it/val_did/anno_1415/index.php";
-// $year = "2014/2015";
-// dip();
+function dip()
+{
+    global $link, $mysqli, $year;
+
+    $arr      = array();
+
+    $xpath    = new DOMXPath(getDOM($link)); // permette di gestire il DOM come se fosse un percorso
+    $lengthN  = $xpath->query('/html/body/table[2]/tr/td/table/tr')->length; // Lunghezza righe (numero di dipartimenti)
+
+    $query    = "INSERT INTO dipartimento (id,nome, tot_cds, tot_moduli,tot_valutati,tot_schedeF,tot_schedeNF) VALUES\n";
+
+    for ($i = 2; $i < $lengthN; $i++) { //prendiamo le info di ogni dipartimento
+
+        $linkDipartimento = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[1]/a')->item(0)->attributes->item(0)->textContent; //Link Dipartimento
+        $_nome            = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[1]')->item(0)->textContent; // Nome dipartimento
+        $_id = parseID($linkDipartimento);
+
+        $_tot_CdS       = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[2]')->item(0)->textContent; // Tot CdS (Corsi di Studio)
+        $_tot_moduli    = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[3]')->item(0)->textContent; // Tot Moduli
+        $_tot_valutati  = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[4]')->item(0)->textContent; // Tot Valutati
+        $_tot_schedeF   = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[6]')->item(0)->textContent; // Totale schede frequentanti
+        $_tot_schedeNF  = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[7]')->item(0)->textContent; // Totale schede NON frequentanti
+        $link_opis      = $xpath->query('/html/body/table[2]/tr/td/table/tr[' . $i . ']/td[8]/a');//->item(0)->attributes->item(0)->textContent; // Link OPIS
+        //$aPathOpis_Dipartimento[$] = $link_opis;
+        echo "\n  ## \033[1m" . ($i-1) . "/" . ($lengthN-2) . "\033[33m\t " . $_nome . "\033[0m";
+        echo "";
+
+        // debugging
+        // if ($_id == 1) {
+        //    echo "\n\n";
+        //    echo $year . "\n";
+        //    echo $link . "\n";
+   
+        //    cds($_id);
+        // }
+
+       cds($_id); // funzione che per ogni dipartimento scorre i suoi corsi di studio
+
+        if (!$mysqli->query('SELECT id FROM dipartimento WHERE id=' . $_id . ';')->num_rows) { //se il dipartimento NON ESISTE
+            $query .= '("' . addslashes($_id) . '","' . addslashes($_nome) . '","' . addslashes($_tot_CdS) . '", "' . addslashes($_tot_moduli) . '", "' . addslashes($_tot_valutati) . '", "' . addslashes($_tot_schedeF) . '", "' . addslashes($_tot_schedeNF) . '"),';
+            $query .= "\n"; // lo inseriamo nel DB
+        }
+
+    }
+
+    $query = substr($query, 0, -2);
+    $query .= ";";
+
+    $endQuery = substr($query, strlen($query) - 6);
+    if ($endQuery != 'VALUE;')
+        if (!$mysqli->query($query))
+            die($mysqli->error);
+    return $arr;
+}
+
+ $link = "http://nucleo.unict.it/val_did/anno_1314/index.php";
+ $year = "2013/2014";
+ olddip();
+
+ $link = "http://nucleo.unict.it/val_did/anno_1415/index.php";
+ $year = "2014/2015";
+ olddip();
 
 // $link = "http://nucleo.unict.it/val_did/anno_1516/index.php";
 // $year = "2015/2016";
-// dip();
+// olddip();
 
-$link = "http://nucleo.unict.it/val_did/anno_1617/index.php";
-$year = "1617";
-dip();
+//$link = "http://nucleo.unict.it/val_did/anno_1617/index.php";
+//$year = "1617";
+//dip();
 
-$link = "http://nucleo.unict.it/val_did/anno_1718/index.php";
-$year = "1718";
-dip();
+//$link = "http://nucleo.unict.it/val_did/anno_1718/index.php";
+//$year = "1718";
+//dip();
 
 echo "\n";
 
