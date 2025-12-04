02968d2aef2e969505c1d879e229769da10f4b99
Chiara <chiara.zuccaro.dev@gmail.com>
2025-12-04 22:15:49 +0100

    deleted app.html == useless

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
+  template: '<router-outlet />',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('opis-manager');
+  // protected readonly title = signal('opis-manager');
 }
