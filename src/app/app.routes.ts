098893ec50a971620e4c7330918e040471543159
Chiara <chiara.zuccaro.dev@gmail.com>
2025-12-04 22:11:43 +0100

    init routes + pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dipartimento } from './pages/dipartimento/dipartimento';
+import { Info } from './pages/info/info';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: ':idDipartimento', component: Dipartimento },
+  { path: 'info', component: Info },
+  // { path: 'formula', },
+  // { path: 'signup', },
+  // { path: 'login', }
+];
