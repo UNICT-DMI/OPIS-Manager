13c5a68a36b3c1fafbc3977d6b89fc62e64206d0
Chiara <chiara.zuccaro.dev@gmail.com>
2025-12-04 22:15:36 +0100

    created home route + component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { Dipartimento } from './pages/dipartimento/dipartimento';
 import { Info } from './pages/info/info';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
+  { path: 'home', component: Home },
   { path: ':idDipartimento', component: Dipartimento },
   { path: 'info', component: Info },
+  { path: "**", redirectTo:'/home' }
   // { path: 'formula', },
   // { path: 'signup', },
   // { path: 'login', }
