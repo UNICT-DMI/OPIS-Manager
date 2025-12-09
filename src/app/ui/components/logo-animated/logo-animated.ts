45f574d49aef8f6105e0b9122f5ca00aeef99a9a
Chiara <chiara.zuccaro.dev@gmail.com>
2025-12-09 19:11:04 +0100

    format files post run

--- src/app/ui/components/logo-animated/logo-animated.ts
+++ src/app/ui/components/logo-animated/logo-animated.ts
@@ -16,33 +16,34 @@ export class LogoAnimated implements AfterViewInit {
   }
 
   private convertElementRefToNativeElement() {
-    return this.lettersRef.toArray().reverse().map(
-      ref => ref.nativeElement as SVGPathElement
-    );
+    return this.lettersRef
+      .toArray()
+      .reverse()
+      .map((ref) => ref.nativeElement as SVGPathElement);
   }
 
   private prepareAllPaths(letters: SVGPathElement[]) {
-    letters.forEach(letterRef => {
+    letters.forEach((letterRef) => {
       const len = letterRef.getTotalLength();
       gsap.set(letterRef, {
         strokeDasharray: len,
-        strokeDashoffset: len
+        strokeDashoffset: len,
       });
-    })
+    });
   }
 
   private setTimeline(nativeLetters: SVGPathElement[]) {
-    const timeline = gsap.timeline({ defaults: { ease: 'back.out'} });
+    const timeline = gsap.timeline({ defaults: { ease: 'back.out' } });
 
     nativeLetters.forEach((letter, i) => {
       timeline.to(letter, { strokeDashoffset: 0, duration: 2 }, i * 0.12);
-    })
+    });
   }
 
   private animateLetters() {
-    if(!this.lettersRef) return;
+    if (!this.lettersRef) return;
     const nativeLetters = this.convertElementRefToNativeElement();
-    
+
     this.prepareAllPaths(nativeLetters);
     this.setTimeline(nativeLetters);
   }
