d718160c2282cee1f4139e98106a83c9da516582
Chiara <chiara.zuccaro.dev@gmail.com>
2025-12-14 20:11:45 +0100

    test(): implemented home test file

--- src/app/pages/home/home.spec.ts
+++ src/app/pages/home/home.spec.ts
@@ -1,22 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { describe, it, expect } from 'vitest';
 import { Home } from './home';
+import { DepartmentsService } from '@services/departments.service';
+import { signal } from '@angular/core';
 
 describe('Home', () => {
   let component: Home;
   let fixture: ComponentFixture<Home>;
 
+  const canStartUserFlow = signal(false);
+  const mockResource = {
+    isLoading: () => false,
+    hasValue: () => true,
+    value: () => [],
+    error: () => null
+  };
+  const mockDepartmentService = {
+    canStartUserFlow,
+    getDepartmentByYear: () => mockResource
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Home],
+      providers: [
+        { provide: DepartmentsService, useValue: mockDepartmentService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
-    await fixture.whenStable();
   });
 
   it('[HOME]: Created', () => expect(component).toBeTruthy());
 
-  
+  it('[HOME]: should reflect canStartUserFlow', () => {
+    canStartUserFlow.set(true);
+    expect(component.canShowDepartments()).toBe(true);
+
+    canStartUserFlow.set(false);
+    expect(component.canShowDepartments()).toBe(false);
+  });
+
+  it('[HOME]: reset canStartUserFlow on Destroy', () => {
+    canStartUserFlow.set(true);
+    component.ngOnDestroy();
+    expect(canStartUserFlow()).toBe(false);
+  })
 });
