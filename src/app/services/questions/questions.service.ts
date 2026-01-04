2f4f112b8f8d8551ea3e8cc526659ddc888a5dd0
Chiara <chiara.zuccaro.dev@gmail.com>
2026-01-04 22:45:20 +0100

    feat(): cache on api /domande

--- src/app/services/questions/questions.service.ts
+++ src/app/services/questions/questions.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
 import { Question } from "@interfaces/question.interface";
-import { catchError, map, switchMap, tap, throwError } from "rxjs";
+import { catchError, map, shareReplay, tap, throwError } from "rxjs";
 import { env } from "src/enviroment";
 
 @Injectable({ providedIn: 'root' })
@@ -9,23 +9,24 @@ export class QuestionService {
   private readonly BASE_URL = env.api_url + '/domande';
   private readonly _http = inject(HttpClient);
 
+  private readonly _questionWeights$ = this._http.get<Question[]>(this.BASE_URL).pipe(
+    map(weights => {
+      if (!weights || weights.length === 0) {
+        throw new Error('Risultati assenti!');
+      }
+      return weights;
+    }),
+    tap(weights => this.questionWeights = weights),
+    shareReplay({ bufferSize: 1, refCount: false }),
+    catchError(() =>
+      throwError(() => new Error('Recupero dei pesi delle domande fallito'))
+    )
+  );
+
   public questionWeights: Question[];
 
-  private getQuestionWeights() {
-    return this._http.get<Question[]>(this.BASE_URL);
-  }
-
   public loadQuestionsWeights() {
-    return this.getQuestionWeights().pipe(
-      map(weights => {
-        if(!weights || weights.length === 0) throw new Error('Risultati assenti!');
-        return weights;
-      }),
-      tap(weights => this.questionWeights = weights),
-      catchError(
-        err => throwError(() => new Error('Recupero dei pesi delle domande fallito'))
-      )
-    );
+    return this._questionWeights$;
   }
 
   // TODO ???
