78aade5bac5730cee661afeb45e72de38f48d922
Chiara <chiara.zuccaro.dev@gmail.com>
2026-01-16 19:58:38 +0100

    refactor(info.ts): name convention + changeDetection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { DepartmentPageComponent } from './pages/department/department';
-import { Info } from './pages/info/info';
+import { InfoPageComponent } from './pages/info/info';
 import { HomePageComponent } from './pages/home/home';
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: ':depsName', component: DepartmentPageComponent },
-  { path: 'info', component: Info },
+  { path: 'info', component: InfoPageComponent },
   { path: '**', redirectTo: '/home' },
   // { path: 'formula', },
   // { path: 'signup', },
--- src/app/pages/info/info.ts
+++ src/app/pages/info/info.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'opis-info',
   templateUrl: './info.html',
   styleUrl: './info.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Info {}
+export class InfoPageComponent {}
