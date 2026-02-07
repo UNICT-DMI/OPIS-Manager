ba6c1d0d9ed663456a488c8e57dd221f131874a9
Chiara <chiara.zuccaro.dev@gmail.com>
2026-02-07 18:13:51 +0100

    fix: path route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,11 @@ import { HomePageComponent } from './pages/home/home';
 import { FormulaComponent } from './pages/formula/formula';
 
 export const routes: Routes = [
-  { path: 'home', component: HomePageComponent },
+  { path: '', component: HomePageComponent },
   { path: 'formula', component: FormulaComponent },
   { path: 'info', component: InfoPageComponent },
   // { path: 'login', }
   // { path: 'signup', },
-  { path: ':depsName', component: DepartmentPageComponent },
-  { path: '**', redirectTo: '/home' },
+  { path: 'department/:depsName', component: DepartmentPageComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
