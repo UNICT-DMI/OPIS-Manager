50fe9fdd12a1b2c85faf8a13040e5ba5e20198c7
Chiara <chiara.zuccaro.dev@gmail.com>
2026-02-08 14:31:21 +0100

    feat: top scrolling on change route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      })
+    ),
     provideCharts(withDefaultRegisterables()),
   ],
 };
