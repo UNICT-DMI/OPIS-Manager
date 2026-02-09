e2ea9aeebc37e4c04ca6cd38e268d71adcdfa657
Chiara <chiara.zuccaro.dev@gmail.com>
2026-02-09 23:48:52 +0100

    fix: footer dimensions

--- src/app/ui/sections/footer/footer.ts
+++ src/app/ui/sections/footer/footer.ts
@@ -1,12 +1,10 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  computed,
   inject,
   OnInit,
   signal,
 } from '@angular/core';
-import { GitHubService } from '@services/github/github.service';
 import { IconRegistryService } from '@services/icon-registry/icon-registry.service';
 import { IconComponent } from '@shared-ui/icon/icon';
 
@@ -18,17 +16,11 @@ import { IconComponent } from '@shared-ui/icon/icon';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Footer implements OnInit {
-  private readonly _githubService = inject(GitHubService);
   private readonly _iconRegistryService = inject(IconRegistryService);
 
-  protected readonly contributorsView = computed(this._githubService.contributors);
   protected readonly githubIcon = signal<string>('');
 
   ngOnInit(): void {
-    this._githubService
-      .getRepoContributors()
-      .then((res) => this._githubService.contributors.set(res));
-
     this._iconRegistryService.load('github').subscribe((icon) => {
       this.githubIcon.set(icon);
     });
